0029a4e4a971e4ace3d53d09b4dd6e4a89a0db11
Kat <mercier200012@hotmail.com>
2025-01-12 16:03:40 -0500

    Make template for profile, added navigation and permissions to viewing own profile.

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+import '../css/Profile.css';
+import { useAppSelector } from "../app/hooks";
+import { useParams } from "react-router-dom";
+
+const Profile = () => {
+    
+    const { username } = useParams();
+    const user = useAppSelector(state => state.users);
+
+    document.title = `${user.username}'s Profile - QuizIt`;
+
+    /* Add different layout for logged in user VS looking at user profile */
+
+    if (username) {
+        return (
+            <div id="profile-page">
+                <Header />
+                <div id="profile-container">
+                    
+                    <div id="user-information">
+                        {/* ADD icon here */}
+                        <div>${user.username}'s Profile</div>
+                        {/* Potential options to add more details? Bio? Status? Created on? */}
+                    </div>
+                    <div className="vert-separator" />
+                    <div id="user-statistics">
+                        {/* Quizzes created? Quizzes completed (number form)? Favorite Quizzes? */}
+                    </div>
+                </div>
+                <Footer />
+            </div> 
+        )
+    }
+    return (
+        <div id="profile-page">
+            <Header />
+            <div id="profile-container">
+                
+                <div id="user-information">
+                    {/* ADD icon here */}
+                    <div>${user.username}'s Profile</div>
+                    {/* Potential options to add more details? Bio? Status? Created on? */}
+                </div>
+                <div className="vert-separator" />
+                <div id="user-statistics">
+                    {/* Quizzes created? Quizzes completed (number form)? Favorite Quizzes? */}
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Profile;
