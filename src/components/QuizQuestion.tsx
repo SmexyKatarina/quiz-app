64208945c50dd7b162546f9f4883254fc8ce8d72
Kat <mercier200012@hotmail.com>
2025-01-18 23:10:00 -0500

    Previously added commit with Quiz.tsx being changed for a functional component

--- src/components/QuizQuestion.tsx
+++ src/components/QuizQuestion.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import QuestionInput from "./QuestionInput";
 
-const Component = () => {
+const QuizQuestion = (props: { questionNumber: number, questionText: string, answerType: string, possibleAnswers: string[] }) => {
     
+    const { questionNumber, questionText, answerType, possibleAnswers } = props;
+
+
     return (
-        <div></div>
+        <div className="quiz-question-container">
+            <h3>{questionNumber} - {questionText}</h3>
+            <QuestionInput questionNumber={questionNumber} answerType={answerType} possibleAnswers={possibleAnswers} />
+        </div>
     );
 }
 
-export default Component;
+export default QuizQuestion;
