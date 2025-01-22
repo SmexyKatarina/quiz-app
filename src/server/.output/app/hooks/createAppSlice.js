a6f43b6ba8995ee459d97d63a8a225bbfe3ffca4
Kat <mercier200012@hotmail.com>
2025-01-22 15:20:50 -0500

    Delete server output files because its for testing purposes not for use

--- src/server/.output/app/hooks/createAppSlice.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAppSlice = void 0;
-const toolkit_1 = require("@reduxjs/toolkit");
-exports.createAppSlice = (0, toolkit_1.buildCreateSlice)({
-    creators: { asyncThunk: toolkit_1.asyncThunkCreator },
-});
